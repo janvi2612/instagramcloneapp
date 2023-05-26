e446c392e27d9cb55eb31e25186089c5203d0da4
HarshMZignuts <122282938+HarshMZignuts@users.noreply.github.com>
2023-05-26 18:42:03 +0530

    Login And Registration Layout done

--- app/src/main/java/com/example/instagramclone/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/com/example/instagramclone/ui/auth/login/LoginFragment.kt
@@ -6,17 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.instagramclone.R
+import com.example.instagramclone.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
-
+    private var _binding : FragmentLoginBinding ?= null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
